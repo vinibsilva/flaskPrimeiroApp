d2fdb1f881c89db6fecb7bde757108eef894df2f
Vinícius Barbosa <106791858+vinibsilva@users.noreply.github.com>
2025-05-28 16:42:57 -0300

    Add files via upload

--- app/__init__.py
+++ app/__init__.py
@@ -1,21 +1,21 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from .config import Config
+from flasgger import Swagger
 
 db = SQLAlchemy()
 
-def create_app():
+def create_app(config_class=Config):
     app = Flask(__name__)
-    app.config.from_object(Config)
+    app.config.from_object(config_class)
     db.init_app(app)
-    
+
     with app.app_context():
         from .models import book_model
         db.create_all()
-        
+        Swagger(app)
+
         from .routes.book_routes import book_bp
         app.register_blueprint(book_bp)
-    
-    return app
 
-   
+    return app
--- app/config.py
+++ app/config.py
@@ -1,3 +1,7 @@
 class Config:
     SQLALCHEMY_DATABASE_URI = "sqlite:///project.db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+class TestingConfig(Config):
+    SQLALCHEMY_DATABASE_URI = "sqlite:///:memory:"
+    TESTING = True
